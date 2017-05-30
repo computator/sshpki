fc6c79861f224ddaff98306da5639fb1aaf978c5
Reuben Lifshay <rlifshay@gmail.com>
2017-05-30 04:37:35 -0700

    Implement the sign_cert method.

--- sshpki.py
+++ sshpki.py
@@ -1,8 +1,13 @@
 import os
+import shutil
 import errno
 import fcntl
 import tempfile
 import subprocess
+import re
+
+class FileNotFoundError(Exception):
+    pass
 
 def _get_new_serial(pki_root):
     with open(os.path.join(os.path.abspath(pki_root), '.serial.lock'), 'w') as lockf:
@@ -23,7 +28,7 @@ def _get_new_serial(pki_root):
     return old_serial + 1
 
 def _sign_key(ca_key, key, identity, serial, principals=None, validity=None, host_key=False):
-    args = ['ssh-keygen', '-s', ca_key, '-I', identity, '-z', str(serial)]
+    args = ['ssh-keygen', '-q', '-s', ca_key, '-I', identity, '-z', str(serial)]
     if host_key:
         args.append('-h')
     if principals:
@@ -34,8 +39,39 @@ def _sign_key(ca_key, key, identity, serial, principals=None, validity=None, hos
     subprocess.check_call(args)
     return key + '-cert.pub'
 
+def _get_fingerprint(keyfile):
+    output = subprocess.check_output(['ssh-keygen', '-l', '-f', keyfile])
+    fingerprint = re.match(r'\d+\s+(?:(?:SHA256|MD5):)?([^\s]+)', output).group(1).replace(':', '')
+    return fingerprint.replace('+', '-').replace('/', '_')
+
 def sign_key(pki_root, ca_key, key, identity, principals, validity, host_key=False, key_is_str=False):
-    pass
+    if not os.path.isdir(pki_root):
+        raise FileNotFoundError("'{}' does not exist".format(pki_root))
+    if not os.path.isfile(ca_key):
+        raise FileNotFoundError("'{}' does not exist".format(ca_key))
+    if not key_is_str and not os.path.isfile(key):
+        raise FileNotFoundError("'{}' does not exist".format(key))
+
+    certsdir = os.path.join(os.path.abspath(pki_root), 'certs')
+    if not os.path.isdir(certsdir):
+        os.mkdir(certsdir)
+
+    if not key_is_str:
+        with open(key, 'r') as keyfile:
+            key = keyfile.read(4096)
+
+    tmpdir = tempfile.mkdtemp(dir=pki_root)
+    try:
+        with tempfile.NamedTemporaryFile('w', dir=tmpdir, delete=False) as key_copy:
+            key_copy.write(key)
+            keyfile = key_copy.name
+        certfile = os.path.join(certsdir, _get_fingerprint(keyfile))
+        out_cert = _sign_key(ca_key, keyfile, identity, _get_new_serial(pki_root), principals, validity, host_key)
+        os.rename(out_cert, certfile)
+    finally:
+        shutil.rmtree(tmpdir)
+
+    return certfile
 
 def find_cert(pki_root, key, key_is_str=False):
     pass
