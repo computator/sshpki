32ffd568b249dab0d2ab9d92a82d7e8043d468a3
Reuben Lifshay <rlifshay@gmail.com>
2017-06-02 13:26:20 -0700

    Rearrange to make installable.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+        name='sshpki',
+        description="Manage a SSH certificate based PKI tree",
+        url='https://github.com/rlifshay/sshpki',
+        packages=['sshpki']
+    )
